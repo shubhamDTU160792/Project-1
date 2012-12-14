5b7b77f0bf6fe86a3209e304fad845b0e58b2702
dtutrainee <shubhamsharma160792@gmail.com>
2012-12-14 22:38:54 +0530

    diff functions

--- Project2.rb
+++ Project2.rb
@@ -22,6 +22,7 @@ end
 def discount
 	
 	puts "Since you are our employee, you will be getting 30% on non-grocery items.Also you will be getting 5% discount on total bill."	
+	
 	@bill_amt=@orig
 	@ng_amt=(@bill_amt)-(@groc_amt)
 	@ng_amt= (0.7)*(@ng_amt)
@@ -40,11 +41,9 @@ def initialize(n,a,g,o,yoj,aff)
 super(n,a,g,o)	
 end
 
-def discount
-	
 
-	if aff==1
-       	@bill_amt=@orig
+def af
+	@bill_amt=@orig
 	@ng_amt=(@bill_amt)-(@groc_amt)
 	@ng_amt= (0.9)*(@ng_amt)
 	@bill_amt=(@ng_amt)+(@groc_amt)
@@ -52,16 +51,11 @@ def discount
 	@b=@a*5
 	@bill_amt=(@bill_amt)-@b
 	puts "Since your an affliated customer, you will be getting 10% on non-grocery items.Also you will be getting 5% discount on total bill."	
-
 	puts "Total bill is : #@bill_amt"
+end
 
-	else
-	
-	if yoj<2010
-	
-	puts "Since your our customer for last 2 years, you will be getting 5% on non-grocery items.Also you will be getting 5% discount on total bill."	
-
-	@bill_amt=@orig
+def old
+@bill_amt=@orig
 	@ng_amt=(@bill_amt)-(@groc_amt)
 	@ng_amt= (0.95)*(@ng_amt)
 	@bill_amt=(@ng_amt)+(@groc_amt)
@@ -69,19 +63,37 @@ def discount
 	@b=@a*5
 	@bill_amt=(@bill_amt)-@b
 	puts "Total bill is : #@bill_amt"
-	else
+
+end
+
+def new1
+
 	@bill_amt=@orig
 	@a=(@bill_amt)/100
 	@b=@a*5
 	@bill_amt=(@bill_amt)-@b
         puts "As you are shopping with us you will be getting 5% discount on total bill."	
-
 	puts "Total bill is : #@bill_amt"
 end
+
+def discount
+	
+
+	if aff==1
+       	af
+
+	else
+	
+	if yoj<2010
+	
+	puts "Since your our customer for last 2 years, you will be getting 5% on non-grocery items.Also you will be getting 5% discount on total bill."	
+	old
+	else
+	new1	
 end
 end
 
-
+end
 puts "Enter 1 for employee and 0 for a regular customer"
 choice = gets.to_i
 
@@ -96,7 +108,7 @@ employee=Employee.new("Sagar",20,500,1000,1238)
 	puts " Employee id is: #{employee.id}"
 	puts " Groceries bill is:#{employee.groc_amt}"
 	puts " total bill is:#{employee.orig}"
-	employee.discount()
+	employee.discount
 	puts " THANK YOU FOR SHOPPING WITH US"	
 
 else
@@ -108,7 +120,7 @@ customer=Customer.new("ankit",120,500,1000,2012,0)
 	puts " Age is :#{customer.age}"
 	puts " Groceries bill is:#{customer.groc_amt}"
 	puts " total bill is:#{customer.orig}"
-	customer.discount()
+	customer.discount
 	puts " THANK YOU FOR SHOPPING WITH US"
 	
 end
